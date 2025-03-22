2663f033f08a5e98046d582454a5bd4df2480dd4
Stephan Kulla <git.mail@kulla.me>
2025-03-22 13:35:18 +0100

    feat: Add loroIpsum

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import '@picocss/pico/css/pico.min.css'
 import './App.css'
 import React from 'react'
 
+const loroIpsum =
+  'Loro ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
 const App = () => {
   return (
     <main className="content">
@@ -11,7 +14,7 @@ const App = () => {
 }
 
 function Textarea() {
-  const [text, setText] = React.useState('This is an example text.')
+  const [text, setText] = React.useState(loroIpsum)
   const [cursor, setCursor] = React.useState<number | null>(null)
 
   return (
