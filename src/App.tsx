29ae5f6fa8e63b004a5b4aeb2ed3a92949ec18e7
Stephan Kulla <git.mail@kulla.me>
2025-03-22 14:47:47 +0100

    fix: Do not apply recurring changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import '@picocss/pico/css/pico.min.css'
 import './App.css'
-import { useEffect, useId, useState } from 'react'
+import { useEffect, useId, useRef, useState } from 'react'
 
 const loroIpsum =
   'Loro ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -17,12 +17,16 @@ function Textarea() {
   const id = useId()
   const [text, setText] = useState(loroIpsum)
   const [caret, setCaret] = useState<number | null>(null)
+  const lastCaretChange = useRef<number | null>(null)
 
   useEffect(() => {
     function handleSelectionChange() {
       const selection = window.getSelection()
 
-      console.log('selection', selection)
+      if (currentTimestamp() - (lastCaretChange.current ?? 0) < 100) {
+        return
+      }
+
       if (
         selection?.anchorNode &&
         selection.isCollapsed &&
@@ -32,11 +36,10 @@ function Textarea() {
       ) {
         setCaret(selection.anchorOffset)
       } else {
-        console.log(selection?.anchorNode?.nodeName)
-        if (selection?.anchorNode?.nodeName !== 'P') {
-          setCaret(null)
-        }
+        setCaret(null)
       }
+
+      lastCaretChange.current = currentTimestamp()
     }
 
     document.addEventListener('selectionchange', handleSelectionChange)
@@ -64,6 +67,10 @@ function Textarea() {
       <pre>{JSON.stringify({ text, cursor: caret }, undefined, 2)}</pre>
     </>
   )
+}
+
+function currentTimestamp() {
+  return new Date().getTime()
 }
 
 function Caret() {
