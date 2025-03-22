562a0e3619c00da46aa8070ea755932d008dde84
Stephan Kulla <git.mail@kulla.me>
2025-03-22 13:51:40 +0100

    feat: Set cursor via selection API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import '@picocss/pico/css/pico.min.css'
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useId, useState } from 'react'
 
 const loroIpsum =
   'Loro ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -14,16 +14,47 @@ const App = () => {
 }
 
 function Textarea() {
+  const id = useId()
   const [text, setText] = useState(loroIpsum)
   const [cursor, setCursor] = useState<number | null>(null)
+
+  useEffect(() => {
+    function handleSelectionChange() {
+      const selection = window.getSelection()
+      if (
+        selection?.anchorNode &&
+        isCollapsed(selection) &&
+        selection.anchorNode.nodeType === Node.TEXT_NODE &&
+        selection.anchorNode.parentElement?.getAttribute('data-textarea-id') ===
+          id
+      ) {
+        setCursor(selection.anchorOffset)
+      } else {
+        setCursor(null)
+      }
+    }
+
+    document.addEventListener('selectionchange', handleSelectionChange)
+
+    return () => {
+      document.removeEventListener('selectionchange', handleSelectionChange)
+    }
+  }, [id])
 
   return (
     <>
       <h1>Textarea</h1>
-      <p>{text}</p>
+      <p data-textarea-id={id}>{text}</p>
       <h2>Internal state</h2>
       <pre>{JSON.stringify({ text, cursor }, undefined, 2)}</pre>
     </>
+  )
+}
+
+function isCollapsed(selection: Selection) {
+  return (
+    selection.anchorNode === selection.focusNode &&
+    selection.anchorOffset === selection.focusOffset
   )
 }
 
